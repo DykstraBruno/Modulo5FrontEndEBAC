a38e0f0ec80ea74b33550608c174336fa03820cd
bruno dykstra <brunodykstra@gmail.com>
2026-02-12 19:30:52 -0300

    errei tudo

--- script.js
+++ script.js
@@ -29,10 +29,14 @@
 
         cadastrar.addEventListener("click", ()=> {
 
-        document.body.classList.add(cadastrar);
-        localStorage.setItem("cadastrar", novoCadastro);
+        document.body.classList.add("cadastro-realizado");
+        localStorage.setItem("cadastrar", "true");
 
-        })
+        });
         document.addEventListener('DOMContentLoaded', ()=> {
             const cadastro = localStorage.getItem("cadastrar");
-        })
+
+            if(cadastro === "true"){
+                document.body.classList.add("cadastro-realizado");
+            }
+        });
