6275df053553b4be5c672cd60e713753eb5b162a
bruno dykstra <brunodykstra@gmail.com>
2026-02-12 19:11:37 -0300

    atualização

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-//Ouvir o evento de quando o usuario sair do campo de CEP
-document.getElementById("cep").addEventListener("blur", (evento)=> {
-    const elemento = evento.target;
-    const cepInformado = elemento.value;
+    //Ouvir o evento de quando o usuario sair do campo de CEP
+    document.getElementById("cep").addEventListener("blur", (evento)=> {
+        const elemento = evento.target;
+        const cepInformado = elemento.value;
 
-//validar o CEP
-    if(!(cepInformado.length ===8))
-        return;
+    //validar o CEP
+        if(!(cepInformado.length ===8))
+            return;
 
-//Fazer busca no viaCEP
-//Promessa de que o Fetch vai buscar esse recurso
-fetch(`https://viacep.com.br/ws/${cepInformado}/json`)
-    .then(response => response.json())
-    .then(data => {
-        //processamento da página
-    if(!data.error){
-        document.getElementById('logradouro').value =data.logradouro;
-        document.getElementById('bairro').value = data.bairro;
-        document.getElementById('cidade').value = data.localidade;
-        document.getElementById('estado').value = data.uf;
+    //Fazer busca no viaCEP
+    //Promessa de que o Fetch vai buscar esse recurso
+    fetch(`https://viacep.com.br/ws/${cepInformado}/json`)
+        .then(response => response.json())
+        .then(data => {
+            //processamento da página
+        if(!data.erro){
+            document.getElementById('logradouro').value =data.logradouro;
+            document.getElementById('bairro').value = data.bairro;
+            document.getElementById('cidade').value = data.localidade;
+            document.getElementById('estado').value = data.uf;
 
-    }else{
-        alert("CEP não encontrado.")
-    }
+        }else{
+            alert("CEP não encontrado.")
+        }
+        })
+        .catch(error => console.error("Erro ao buscar o CEP> " , error));
     })
-    .catch(error => console.error("Erro ao buscar o CEP> " , error));
+    const cadastrar = document.getElementById("cadastrar");
 
- })
+        cadastrar.addEventListener("click", ()=> {
+
+        document.body.classList.add(cadastrar);
+        localStorage.setItem("cadastrar", novoCadastro);
+
+        })
+        document.addEventListener('DOMContentLoaded', ()=> {
+            const cadastro = localStorage.getItem("cadastrar");
+        })
